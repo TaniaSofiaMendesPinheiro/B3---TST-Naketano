18fe7349571c5475dd8bf2a2730f26e6b1134ca5
TobiasBahlo <tobias.bahlo@student.reutlingen-university.de>
2015-10-26 12:04:44 +0100

    Klasserümpfe der KI hinzugefügt -Komposition mit Spieler zum Teil fertig

---
+++ DameB3/src/Basisklassen/KI.java
@@ -0,0 +1,16 @@
+package Basisklassen;
+
+public abstract class KI {
+	
+	private Spieler spieler;
+	
+	public KI(Spieler spieler){
+		if(spieler == null){
+			throw new RuntimeException("KI can`t exist without a Spieler");
+		}
+		else{
+			this.spieler = spieler;
+		}
+	}
+
+}
---
+++ DameB3/src/Basisklassen/KI_Dame.java
@@ -0,0 +1,14 @@
+package Basisklassen;
+
+public class KI_Dame extends KI{
+
+	public KI_Dame(Spieler spieler) {
+		super(spieler);
+		
+	}
+	
+	
+	
+
+
+}
