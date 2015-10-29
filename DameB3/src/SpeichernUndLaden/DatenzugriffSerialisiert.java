1bc0756e3aa217ae98b33c4e7fee4c3f31aacce1
fetter94 <Sina.Fetter@Student.Reutlingen-University.de>
2015-10-29 14:37:20 +0100

    __

--- DameB3/src/SpeichernUndLaden/DatenzugriffSerialisiert.java
+++ DameB3/src/SpeichernUndLaden/DatenzugriffSerialisiert.java
@@ -2,12 +2,40 @@ package SpeichernUndLaden;
 
 import java.io.Serializable;
 
-public class DatenzugriffSerialisiert implements Serializable{
+public class DatenzugriffSerialisiert implements Serializable, iDatenzugriff{
 
 	/**
 	 * default serialnr.
 	 */
 	private static final long serialVersionUID = 1L;
+
+	
+	/**
+	 * method for SpielLaden()
+	 * saved positions and moves are on screen
+	 */
+	@Override
+	public void spielLaden() {
+	
+	}
+	
+	/**
+	 * this method saves the game from the moment you save
+	 */
+
+	@Override
+	public void spielSpeichern() {
+	
+	}
+
+	/**
+	 * this method saves the positions of your figures, if you want to go out for a while
+	 * 
+	 */
+	@Override
+	public void speicherPositionenVonFigur() {
+	
+	}
 	
 
 }
