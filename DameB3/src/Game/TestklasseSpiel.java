7b476f7b82563c237e9a492f7c133400e19f30b7
fetter94 <Sina.Fetter@Student.Reutlingen-University.de>
2015-11-04 15:26:03 +0100

    testklasse spiel

---
+++ DameB3/src/Game/TestklasseSpiel.java
@@ -0,0 +1,18 @@
+package Game;
+
+import Basisklassen.FarbEnum;
+import Basisklassen.Spielbrett;
+
+public class TestklasseSpiel {
+
+	public static void main(String[] args) {
+
+		Spiel s2 = new Spiel();
+		s2.spielerHinzufügen("Sina", "weiss", false);
+		s2.spielerHinzufügen("Kol", "schwarz", false);
+		s2.zugDurchführen("a5", "b6");
+		s2.updateFeld();
+		System.out.println(s2);
+	}
+
+}
