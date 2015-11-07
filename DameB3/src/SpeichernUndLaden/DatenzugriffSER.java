b2878e8c1b0be67e26694bf1d611d53d2e39be86
fetter94 <Sina.Fetter@Student.Reutlingen-University.de>
2015-11-07 12:17:43 +0100

    speichern&laden / spiel

--- DameB3/src/SpeichernUndLaden/DatenzugriffSER.java
+++ DameB3/src/SpeichernUndLaden/DatenzugriffSER.java
@@ -1,9 +1,11 @@
 package SpeichernUndLaden;
 
-import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Properties;
 
 //Implementieren Sie das Interface iDatenzugriff in der Klasse DatenzugriffSER , bei dem ein Spiel mit seinem
 //gesamten Zustand serialisiert gespeichert und geladen werden kann.
@@ -12,9 +14,34 @@ public class DatenzugriffSER implements iDatenzugriff {
 	private ObjectInputStream ois;
 	private ObjectOutputStream oos;
 
+	@Override
+	public void open(Properties properties) throws IOException {
+		String dateiName = properties.getProperty("Filename");
+
+		if (dateiName == null) {
+			throw new IOException("Filename not defined!");
+		} else {
+			if ("s".equals(properties.getProperty("Mode"))) {
+				oos = new ObjectOutputStream(new FileOutputStream(dateiName));
+			} else if ("l".equals(properties.getProperty("Mode"))) {
+				ois = new ObjectInputStream(new FileInputStream(dateiName));
+			} else {
+				throw new IOException("Mode not defined");
+			}
+		}
+	}
 
 	@Override
-	public Object laden() throws IOException {
+	public void write(Object object) throws IOException {
+		if (oos == null) {
+			throw new IOException("Stream to write not is not open!");
+		} else {
+			oos.writeObject(object);
+		}
+	}
+
+	@Override
+	public Object read() throws IOException {
 		if (ois == null) {
 			throw new IOException("Stream to read is not open!");
 		}
@@ -22,20 +49,19 @@ public class DatenzugriffSER implements iDatenzugriff {
 		try {
 			Object object = ois.readObject();
 			return object;
-			
 		} catch (ClassNotFoundException error) {
 			throw new IOException("Could not deserialize!");
 		}
 	}
 
 	@Override
-	public void speichern(String pfad, String name, String inhalt) throws IOException {
-		
-		if ( oos == null){
-			throw new IOException("Stream to write on is not open!");
-		} else{
-			oos.writeObject(inhalt); 
+	public void close(Object object) throws IOException {
+		if (ois != null) {
+			ois.close();
+			ois = null;
+		} else if (oos != null) {
+			oos.close();
+			oos = null;
 		}
-		
 	}
 }
