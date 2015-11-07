26b36e1876e3053c3013580cf72a6fdb79193322
fetter94 <Sina.Fetter@Student.Reutlingen-University.de>
2015-11-07 16:14:47 +0100

    16:14

--- DameB3/src/Game/Spiel.java
+++ DameB3/src/Game/Spiel.java
@@ -484,10 +484,6 @@ public class Spiel implements iBediener {
 		int[] liste2 = brett.getIndexById(zielId);
 
 		if (brett.gibMirDiePosition(startId).getSpielfigur().getFarbEnum() == FarbEnum.weiss){
-		return (Math.abs(liste2[0]) - Math.abs(liste1[0]) == 2 && liste2[1] - liste1[1] == 2 ||
-				Math.abs(liste2[0]) - Math.abs(liste1[0]) == -2 && liste2[1] - liste1[1] == 2 || 
-				Math.abs(liste2[0]) - Math.abs(liste1[0]) == 2 && liste2[1] - liste1[1] == -2 ||
-				Math.abs(liste2[0]) - Math.abs(liste1[0]) == -2 && liste2[1] - liste1[1] == -2); 
 		if (Math.abs(liste2[0]) - Math.abs(liste1[0]) == 2 && liste2[1] - liste1[1] == 2){
 			return true;
 		} else if (Math.abs(liste2[0]) - Math.abs(liste1[0]) == -2 && liste2[1] - liste1[1] == 2){
@@ -497,14 +493,6 @@ public class Spiel implements iBediener {
 			} else if(Math.abs(liste2[0]) - Math.abs(liste1[0]) == -2 && liste2[1] - liste1[1] == -2) {
 			return true;
 		}
-		
-//		else if (brett.gibMirDiePosition(startId).getSpielfigur().getFarbEnum() == FarbEnum.schwarz){
-		else{
-			return(Math.abs(liste2[0]) - Math.abs(liste1[0]) == -2 && liste2[1] - liste1[1] == 2 ||
-						Math.abs(liste2[0]) - Math.abs(liste1[0]) == -2 && liste2[1] - liste1[1] == -2 ||
-						Math.abs(liste2[0]) - Math.abs(liste1[0]) == 2 && liste2[1] - liste1[1] == -2 ||
-						Math.abs(liste2[0]) - Math.abs(liste1[0]) == 2 && liste2[1] - liste1[1] == 2);
-			
 		}
 		else if (brett.gibMirDiePosition(startId).getSpielfigur().getFarbEnum() == FarbEnum.schwarz){
 				if(Math.abs(liste2[0]) - Math.abs(liste1[0]) == -2 && liste2[1] - liste1[1] == 2){
@@ -516,11 +504,9 @@ public class Spiel implements iBediener {
 		}	else if (Math.abs(liste2[0]) - Math.abs(liste1[0]) == 2 && liste2[1] - liste1[1] == 2) {
 			return true;
 		}
-
 		}
 		return false;
 	}
-
 
 	public boolean prüfeDifDame(String startId, String zielId) {
 		int[] liste1 = brett.getIndexById(startId);
