37f84c2b5dca1e0fcdc36bb51c12bf111b9354e8
fetter94 <Sina.Fetter@Student.Reutlingen-University.de>
2015-11-07 12:14:20 +0100

    laden.speichern

--- DameB3/src/SpeichernUndLaden/iCSV.java
+++ DameB3/src/SpeichernUndLaden/iCSV.java
@@ -1,5 +1,19 @@
 package SpeichernUndLaden;
 
-public class iCSV {
+public interface iCSV {
+	/**
+	 * This method saves the data in a CSV file.
+	 * @param fileName The passed file name.
+	 * @since 1.0.0
+	 */
+	public void saveCSV(String fileName);
+	
+	/**
+	 * Loads the objects in a CSV file.
+	 * @param fileName The passed Filename.
+	 * @return the loaded objects.
+	 * @since 1.0.0
+	 */
+	public Object loadCSV(String fileName);
 
 }
--- DameB3/src/SpeichernUndLaden/iDatenzugriff.java
+++ DameB3/src/SpeichernUndLaden/iDatenzugriff.java
@@ -1,7 +1,7 @@
 package SpeichernUndLaden;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * here are methods which save the data
@@ -13,21 +13,35 @@ import java.io.IOException;
 public interface iDatenzugriff {
 	
 	/**
+	 * This method opens a file.
+	 * @param properties The passed properties.
+	 * @throws IOException is thrown if the file can't be open.
+	 * @since 1.0.0
+	 */
+	public void open(Properties properties) throws IOException;
+	
+	/**
 	 * This method writes the object.
 	 * @param object The passed object.
 	 * @throws IOException is thrown if the object can't be written or the file isn't open.
 	 * @since 1.0.0
 	 */
-	public void speichern(String pfad, String name, String inhalt) throws IOException;
+	public void write(Object object) throws IOException;
 	
 	/**
 	 * Reads the objects in a file.
 	 * @return the loaded objects.
 	 * @throws IOException is thrown if the objects can't be read or the file isn't open.
 	 */
-	public Object laden() throws IOException;
+	public Object read() throws IOException;
 	
-	
+	/**
+	 * This method close a file.
+	 * @param object The passed object.
+	 * @throws IOException is thrown if the file isn't open.
+	 * @since 1.0.0
+	 */
+	public void close(Object object) throws IOException;
 }
 
 
